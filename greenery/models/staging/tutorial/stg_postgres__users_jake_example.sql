399589ed65b39b43bfddf5eedb237efa69eeab4f
Matthias Motl <matthias.motl@neuefische.de>
2023-09-17 21:01:04 +0000

    staging model lecture example

---
+++ greenery/models/staging/tutorial/stg_postgres__users_jake_example.sql
@@ -0,0 +1,25 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+with source as (
+    select * from {{source('postgres', 'users')}}
+)
+
+, renamed_recast as (
+    select
+    user_id as user_guid
+    , first_name
+    , last_name
+    , last_name
+    , email
+    , phone_number
+    , created_at as created_at_utc
+    , updated_at
+    , address_id
+  from source
+)
+
+select * from renamed_recast
